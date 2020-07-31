52e59c319cec6e821e63c53b60591984c962bfdd
joewalker <assassin.joe@hotmail.com>
2020-07-31 20:01:32 +0700

    create maven build and sonar and install dependency

--- vars/Maven.groovy
+++ vars/Maven.groovy
@@ -1,5 +1,18 @@
 #!/bin/groovy
 
 def Build(applicationId = null) {
+    sh "mvn -X package"
+}
+
+def InstallDependency() {
     sh "mvn clean install"
+}
+
+def SonarScan(projectKey = null, projectName = null) {
+    sh """
+        mvn admin:admin \
+          -Dsonar.projectKey=${projectKey} \
+          -Dsonar.projectName=${projectName} \
+          -Dsonar.host.url=http://localhost:9000 \
+    """
 }
