ef489652723b6afce5612331e1dcd0288c6e7748
joewalker <assassin.joe@hotmail.com>
2020-08-08 08:41:26 +0700

    remove sonar webhook secret

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -13,7 +13,7 @@ def Scan(projectKey = null, projectName = null) {
     }
 
     timeout(time: 1, unit: 'HOURS') {
-        def qg = waitForQualityGate(webhookSecretId: "joewalker")
+        def qg = waitForQualityGate()
         if (qg.status != 'OK') {
             error "Pipeline aborted due to quality gate failure: ${qg.status}"
         }
