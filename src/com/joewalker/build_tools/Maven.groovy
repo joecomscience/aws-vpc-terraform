17002402a64302435240632cabcc4b6d04d4058a
joewalker <assassin.joe@hotmail.com>
2020-08-21 13:24:21 +0700

    debug

--- src/com/joewalker/build_tools/Maven.groovy
+++ src/com/joewalker/build_tools/Maven.groovy
@@ -3,11 +3,12 @@ package com.joewalker.build_tools
 class Maven implements Serializable {
     def steps
     Maven(steps) {
+        println '--debug--'
+        println steps.getClass()
         this.steps = steps
     }
 
     void Build() {
-        println(this.steps.getClass())
         this.steps.sh "mvn clean package"
     }
 
